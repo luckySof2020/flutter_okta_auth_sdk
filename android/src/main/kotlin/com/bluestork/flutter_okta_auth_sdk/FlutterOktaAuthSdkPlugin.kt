cc078620f386d16baa69a46102ac269dfe0c01db
luckySof2020 <155450715+luckySof2020@users.noreply.github.com>
2025-05-28 21:35:09 +0200

    Update FlutterOktaAuthSdkPlugin.kt

--- android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/FlutterOktaAuthSdkPlugin.kt
+++ android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/FlutterOktaAuthSdkPlugin.kt
@@ -16,127 +16,116 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.PluginRegistry
 
 /** FlutterOktaAuthSdkPlugin */
+class FlutterOktaAuthSdkPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private var applicationContext: Context? = null
+    private var mainActivity: Activity? = null
 
-class FlutterOktaAuthSdkPlugin : FlutterPlugin, MethodCallHandler,
-  PluginRegistry.ActivityResultListener, ActivityAware {
-  private lateinit var channel: MethodChannel
-
-  private var applicationContext: Context? = null
-  private var mainActivity: Activity? = null
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    this.onAttachedToEngine(flutterPluginBinding.applicationContext, flutterPluginBinding.binaryMessenger)
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-    try {
-      OktaClient.getWebClient().handleActivityResult(requestCode, resultCode, data)
-    }catch (_:Exception){}
-    return PendingOperation.hasPendingOperation != null
-
-  }
-
-  override fun onDetachedFromActivity() {
-    this.mainActivity = null
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    binding.addActivityResultListener(this)
-    mainActivity = binding.activity
-  }
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    binding.addActivityResultListener(this)
-    mainActivity = binding.activity
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    this.mainActivity = null
-  }
-
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
-    val arguments = call.arguments<Map<String, Any>>()
-    PendingOperation.init(call.method, result)
-
-    if (applicationContext == null)
-      PendingOperation.error(Errors.NO_CONTEXT)
-
-    try {
-      when (call.method) {
-        AvailableMethods.CREATE_CONFIG.methodName -> {
-          if (arguments != null) {
-            createConfig(arguments, applicationContext!!)
-          }
-        }
-        AvailableMethods.IS_ACCESS_TOKEN_EXPIRED.methodName -> {
-            isAccessTokenExpired()
-        }
-        AvailableMethods.SIGN_IN.methodName -> {
-          signInNew(this.mainActivity!!)
-        }
-        AvailableMethods.SIGN_OUT.methodName -> {
-          signOut(this.mainActivity!!)
-        }
-        AvailableMethods.GET_USER.methodName -> {
-          getUser()
-        }
-        AvailableMethods.IS_AUTHENTICATED.methodName -> {
-          isAuthenticated()
-        }
-        AvailableMethods.GET_ACCESS_TOKEN.methodName -> {
-          getAccessToken()
-        }
-        AvailableMethods.GET_ID_TOKEN.methodName -> {
-          getIdToken()
-        }
-        AvailableMethods.REVOKE_ACCESS_TOKEN.methodName -> {
-          revokeAccessToken()
-        }
-        AvailableMethods.REVOKE_ID_TOKEN.methodName -> {
-          revokeIdToken()
-        }
-        AvailableMethods.REVOKE_REFRESH_TOKEN.methodName -> {
-          revokeRefreshToken()
-        }
-        AvailableMethods.CLEAR_TOKENS.methodName -> {
-          clearTokens()
-        }
-        AvailableMethods.INTROSPECT_ACCESS_TOKEN.methodName -> {
-          introspectAccessToken()
-        }
-        AvailableMethods.INTROSPECT_ID_TOKEN.methodName -> {
-          introspectIdToken()
-        }
-        AvailableMethods.INTROSPECT_REFRESH_TOKEN.methodName -> {
-          introspectRefreshToken()
-        }
-        AvailableMethods.REFRESH_TOKENS.methodName -> {
-          refreshTokens()
-        }
-        else -> {
-          PendingOperation.error(Errors.METHOD_NOT_IMPLEMENTED, "Method called: $call.method")
-        }
-      }
-    } catch (ex: java.lang.Exception) {
-      PendingOperation.error(Errors.GENERIC_ERROR, ex.localizedMessage)
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        this.onAttachedToEngine(flutterPluginBinding.applicationContext, flutterPluginBinding.binaryMessenger)
     }
-  }
 
-  fun setActivity(activity: Activity) {
-    this.mainActivity = activity
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
-  private fun onAttachedToEngine(context: Context, binaryMessenger: BinaryMessenger) {
-    applicationContext = context
-    channel = MethodChannel(binaryMessenger, "flutter_okta_auth_sdk")
-    channel.setMethodCallHandler(this)
-  }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+        try {
+            OktaClient.getWebClient().handleActivityResult(requestCode, resultCode, data)
+        } catch (_: Exception) {}
+        return PendingOperation.hasPendingOperation != null
+    }
+
+    override fun onDetachedFromActivity() {
+        this.mainActivity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        binding.addActivityResultListener(this)
+        mainActivity = binding.activity
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        binding.addActivityResultListener(this)
+        mainActivity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        this.mainActivity = null
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+        val arguments = call.arguments<Map<String, Any>>()
+        PendingOperation.init(call.method, result)
+
+        if (applicationContext == null) {
+            PendingOperation.error(Errors.NO_CONTEXT)
+            return
+        }
+
+        try {
+            when (call.method) {
+                AvailableMethods.CREATE_CONFIG.methodName -> {
+                    if (arguments != null) {
+                        createConfig(arguments, applicationContext!!)
+                    }
+                }
+                AvailableMethods.SIGN_IN.methodName -> {
+                    signInNew(this.mainActivity!!)
+                }
+                AvailableMethods.SIGN_OUT.methodName -> {
+                    signOut(this.mainActivity!!)
+                }
+                AvailableMethods.GET_USER.methodName -> {
+                    getUser ()
+                }
+                AvailableMethods.IS_AUTHENTICATED.methodName -> {
+                    isAuthenticated()
+                }
+                AvailableMethods.GET_ACCESS_TOKEN.methodName -> {
+                    getAccessToken()
+                }
+                AvailableMethods.GET_ID_TOKEN.methodName -> {
+                    getIdToken()
+                }
+                AvailableMethods.REVOKE_ACCESS_TOKEN.methodName -> {
+                    revokeAccessToken()
+                }
+                AvailableMethods.REVOKE_ID_TOKEN.methodName -> {
+                    revokeIdToken()
+                }
+                AvailableMethods.REVOKE_REFRESH_TOKEN.methodName -> {
+                    revokeRefreshToken()
+                }
+                AvailableMethods.CLEAR_TOKENS.methodName -> {
+                    clearTokens()
+                }
+                AvailableMethods.INTROSPECT_ACCESS_TOKEN.methodName -> {
+                    introspectAccessToken()
+                }
+                AvailableMethods.INTROSPECT_ID_TOKEN.methodName -> {
+                    introspectIdToken()
+                }
+                AvailableMethods.INTROSPECT_REFRESH_TOKEN.methodName -> {
+                    introspectRefreshToken()
+                }
+                AvailableMethods.REFRESH_TOKENS.methodName -> {
+                    refreshTokens()
+                }
+                else -> {
+                    PendingOperation.error(Errors.METHOD_NOT_IMPLEMENTED, "Method called: ${call.method}")
+                }
+            }
+        } catch (ex: Exception) {
+            PendingOperation.error(Errors.GENERIC_ERROR, ex.localizedMessage)
+        }
+    }
+
+    private fun onAttachedToEngine(context: Context, binaryMessenger: BinaryMessenger) {
+        applicationContext = context
+        channel = MethodChannel(binaryMessenger, "flutter_okta_auth_sdk")
+        channel.setMethodCallHandler(this)
+    }
 }
